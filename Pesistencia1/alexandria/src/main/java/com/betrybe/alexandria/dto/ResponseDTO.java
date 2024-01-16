49f718b4e03637e03ed040d34aafa6f4fc96235c
Felipe Santiago <devfelipesantiago@gmail.com>
2024-01-16 15:55:13 -0300

    Init - Persistencia 1

--- Pesistencia1/alexandria/src/main/java/com/betrybe/alexandria/dto/ResponseDTO.java
+++
@@ -1,5 +0,0 @@
-package com.betrybe.alexandria.dto;
-
-public record ResponseDTO<T>(String message, T data) {
-
-}
--- Pesistencia1/alexandria/src/main/java/com/betrybe/alexandria/model/repositiry/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.betrybe.alexandria.model.repositiry;
-
-import com.betrybe.alexandria.model.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
